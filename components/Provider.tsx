89708161e01e3eaecfb0ee8f19244d231e1e2e4a
bhataasim1 <bhataasim700@gmail.com>
2024-04-29 19:18:07 +0530

    ✨feature: Add ModeToggle component and Provider component

---
+++ components/Provider.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from "./theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+}
