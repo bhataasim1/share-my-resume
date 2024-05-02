d9fc7746b5d5f12b7aa0382e4090065c4b0c1dd1
bhataasim1 <bhataasim700@gmail.com>
2024-05-02 20:46:41 +0530

    ✨feature: HeroCard.tsx to use HeroResumeSVG image

--- components/layout/HeroCard.tsx
+++ components/layout/HeroCard.tsx
@@ -1,7 +1,10 @@
+import HeroResumeSVG from "@/public/undraw_online_cv_re_gn0a.svg";
+import Image from "next/image";
+
 export const HeroCards = () => {
   return (
     <>
-      <h1>Here Put some Resume demo</h1>
+      <Image src={HeroResumeSVG} alt="Hero Resume" priority />
     </>
   );
 };
