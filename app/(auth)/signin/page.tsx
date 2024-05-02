126a4504be23892b3cfecaebe2ac4e288824c13c
bhataasim1 <bhataasim700@gmail.com>
2024-05-02 22:27:31 +0530

    🐞 fix: sign-in page UI Design

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/navigation";
 import { dashboard } from "@/constant";
 import { useEffect } from "react";
 import UserLoginForm from "@/components/form/UserLoginForm";
+import Image from "next/image";
+import SignInImage from "@/public/undraw_sign_up_n6im.svg";
 
 export default function LoginPage() {
   const { data: session } = useSession();
@@ -18,63 +20,36 @@ export default function LoginPage() {
   }, [session, router]);
 
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative hidden h-full flex-col bg-muted p-10 lg:flex">
-        <div className="absolute inset-0" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Logo
-        </div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
-            </p>
-            <footer className="text-sm">Sofia Davis</footer>
-          </blockquote>
-        </div>
-      </div>
-      <div className="p-4 lg:p-8 h-full flex items-center">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Login to your account
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-balance text-muted-foreground">
+              Enter your email below to login to your account
             </p>
           </div>
-          <UserLoginForm />
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{" "}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Terms of Service
-            </Link>{" "}
-            and{" "}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
+          <div className="grid gap-4">
+            <UserLoginForm />
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
             </Link>
-            .
-          </p>
+          </div>
+        </div>
+      </div>
+      <div className="hidden bg-muted lg:flex items-center justify-center">
+        <div className="w-3/4">
+          <Image
+            src={SignInImage}
+            alt="Image"
+            className="object-contain" 
+            width={1000}
+            height={1000} 
+            priority
+          />
         </div>
       </div>
     </div>
